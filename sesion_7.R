6293ade897f28956eb5ee9628ad15dcdd8b07064
Gio-Von-derFluss <yovanny_54@hotmail.com>
2021-10-28 10:23:47 -0500

    Add sesion 7 and 8

---
+++ sesion_7.R
@@ -0,0 +1,175 @@
+####################################################################
+##                                                                ##
+##  Análisis de datos utilizando el lenguaje de programación R:   ##
+##  una experiencia de aprendizaje virtual en el aula invertida   ##
+##                                                                ##
+##                 Proyecto INNOVA-TIC 2021                       ##
+##                                                                ##
+## Creado por:     Björn Reu (breu@uis.edu.co )                   ##
+##                 Yovanny Duran Barajas                          ##
+##                 Zarith Tatiana Villamizar                      ##
+##                 Silvia Fernanda Ardila Hurtado                 ## 
+##                 Sergio Andres Bolivar-Santamaria               ##
+##                                                                ##
+##  con el apoyo de la Vicerrectoría Académica,                   ##
+##  Universidad Industrial de Santander                           ##
+##                                                                ##
+####################################################################
+##                                                                ##
+##            ***   Unidad 2  ***  Sesión 1   ***                 ##
+##                                                                ##
+####################################################################
+
+# En R, existen funciones para la generacion de numeros aleatorios, es decir, podremos generar cualquier numero de 0 a infinito de forma automatica. Para esto, debemos especificar la distribucion de probabilidad de la cual queremos obtener estos numeros aleatorios.
+
+# Una distribucion de probabilidad describe la gama de resultados que podemos obtener. De esta manera, la probabilidad de que obtengamos un valor dependera de la distribucion de probabilidad.
+
+# Ejemplo:
+
+# La probabilidad de los dados: 
+# Un dado convencional se caracteriza por poseer 6 lados, y cada lado representa un valor de 1 al 6. Frecuentemente en juegos de mesa como el parques, mientras mas alto sea el numero, mas podremos avanzar y ganar. Al lanzar el dado para poder avanzar, existe una probabilidad fija de obtener cualquiera de los 6 lados. Ya que esta probabilidad es la misma para cada lado (1/6), podriamos decir que la distribucion de los numeros de un dado pertenece a la distribucion uniforme.
+
+###################################################################
+##                  Distribución uniforme                        ##
+###################################################################
+
+# En nuestro ejemplo del dado, planteamos que la distribución de los números de cada lado del dado tiene una distribucion uniforme, debido a que la probabilidad de de obtener cada lado es la misma. Vamos a explorar un poco esto en R.
+
+# Para obtener un numero aleatorio n que viene de una distribucion uniforme utilizamos runif()
+
+?runif
+
+# Vamos a escoger 10 numeros aleatorios del 1 al 6 que vienen de una distribucion uniforme
+
+ru1 <- runif(n = 10, min = 1, max = 6) #los numeros que obtendremos tienen decimales
+
+hist(ru1)
+
+# Segun nuestro histograma, y dependiendo de los numeros que nos toque, puede que obtengamos mas veces un numero que otro, ¿entonces la probabilidad no es la misma?
+
+ru2 <- runif(n =100, min = 1, max = 6) 
+hist(ru2)
+
+ru3 <- runif(n =1000, min = 1, max = 6) 
+hist(ru3)
+
+# ¡Ahora nuestro histograma parece más uniforme!. Cuando trabajamos con datos, siempre es una buena practica tener una muestra grande, con esto evitamos el sesgo en nuestros analisis.
+
+###################################################################
+##                    Distribución normal                        ##
+###################################################################
+
+# La distribucion normal es una de las más importantes, ya que se ajusta a muchos datos que representan procesos en la vida real. Su representacion se asemeja a la de una campana, la campana de Gauss.
+
+# Para obtener un numero aleatorio n que viene de una distribuion normal utilizamos rnorm()
+
+?rnorm
+rnorm(n = 4, mean = 100, sd = 5)
+
+# ?set.seed para reproducir los mismos numeros
+
+set.seed(6)
+rnorm(n = 10, mean = 5, sd = 5)
+set.seed(6)
+rnorm(n = 10, mean = 5, sd = 5)
+set.seed(4)
+rnorm(n = 10, mean = 5, sd = 5)
+
+# Distribucion normal estandar
+
+?rnorm
+rn<-rnorm(n = 100)
+rn<-rnorm(n = 1000, mean = 0, sd = 1)
+rn
+hist(rn)
+mean(rn)
+sd(rn)
+
+# Distribución normal con media de 5 y distribución estandar de 2
+
+rn<-rnorm(n = 100, mean = 5, sd = 2)
+hist(rn)
+mean(rn)
+sd(rn)
+
+# Distribucion normal con media de 5 y distribucion estandar de 2
+
+# Aumente n: 1000, 10000, 100000
+
+r<-rnorm(1000000, mean=5, sd=2)
+hist(r)
+mean(r)
+sd(r)
+
+# Vamos a cargar un conjunto de datos que contiene dos medidas del sepalo y petalo de diferentes especies de flores
+
+data(iris)
+hist(iris$Sepal.Length)
+hist(iris$Sepal.Width)
+hist(iris$Petal.Length)
+hist(iris$Petal.Width)
+
+# Tydi
+
+library(ggplot2)
+
+ggplot(iris, aes(Sepal.Width)) +
+  geom_bar(stat = "count")
+
+# ¿Cuál dato representa mejor una distribución normal?
+
+##################################################################
+## Ejercicio
+
+# 1. Cree un vector v1 que contenga una muestra de 1000 numeros aleatorios que vengan de una distribucion normal con media de 112 y desviacion estandar  de 35. Grafique los valores en un histograma
+
+v1 <- rnorm(n = 1000, mean = 112, sd = 35)
+hist(v1)
+
+# 2. Cree un vector v2 que contenga una muestra de 1000 numeros aleatorios que vengan de una distribucion normal con media de 34 y desviacion estandar de 3. Grafique los valores en un histograma
+
+v2 <- rnorm(n = 1000, mean=34, sd=3)
+hist(v2)
+
+# 3. Multiplique v1 con v2 y grafique el resutado en un histograma. ¿Cual es la diferencia de este histograma con los anteriores?
+
+v3 <- v1*v2
+hist(v3, 50)
+
+# 4. Cree un vector "x" que contenga 14 numeros entre 1 y 50 que vengan de una distribucion uniforme. Cree otro vector "y" que contenga los valores de "x" pero dos veces mas grande. Suma a cada valor de "y" un valor constante de 7. Ahora grafiquelo mostrando "y" sobre "x" : plot(x,y)
+
+x <- runif(n = 15, min = 1, max = 50)
+y <- (2*x)+7
+plot(x,y)
+
+## fin ejercicio
+############################################################################
+
+###################################################################
+##           Probabilidad en la distribución normal              ##
+###################################################################
+
+# Para calcular la probabilidad de obtener un valor que viene de una distribución normal, utilizamos la funcion pnorm()
+
+?pnorm
+
+# Podemos calcular la probabilidad de obtener un valor menor que -2 que venga de una distribución normal (mean = 2, sd = 2)
+
+pnorm(q = 9, mean = 2, sd = 2)
+
+# O también podemos calcular la probabilidad de obtener un valor mayor que -2 que venga de la distribucion normal mencionada anteriormente (mean = 2, sd = 2)
+
+1-pnorm(-2,mean=2,sd=2)
+
+##################################################################
+## Ejercicio
+
+# 1) ¿Cuál es la probabilidad de obtener un valor menor que 5 que venga de la distribucion normal mencionada anteriormente (mean = 5, sd = 2)?
+
+pnorm(q = 100,m = 0,sd = 1)
+
+# 2) ¿Cuál es la probabilidad de obtener un valor entre 2 y 8?
+pnorm(8,5,2)-pnorm(2,5,2)
+
+## fin ejercicio
+############################################################################
---
+++ sesion_8.R
@@ -0,0 +1,191 @@
+####################################################################
+##                                                                ##
+##  Análisis de datos utilizando el lenguaje de programación R:   ##
+##  una experiencia de aprendizaje virtual en el aula invertida   ##
+##                                                                ##
+##                 Proyecto INNOVA-TIC 2021                       ##
+##                                                                ##
+## Creado por:     Björn Reu (breu@uis.edu.co )                   ##
+##                 Yovanny Duran Barajas                          ##
+##                 Zarith Tatiana Villamizar                      ##
+##                 Silvia Fernanda Ardila Hurtado                 ## 
+##                 Sergio Andres Bolivar-Santamaria               ##
+##                                                                ##
+##  con el apoyo de la Vicerrectoría Académica,                   ##
+##  Universidad Industrial de Santander                           ##
+##                                                                ##
+####################################################################
+##                                                                ##
+##            ***   Unidad 2  ***  Sesión 2   ***                 ##
+##                                                                ##
+####################################################################
+
+# En esta sesión vamos a aprender a realizar el test de análisis de varianza o ANOVA. Mediante esta prueba, se busca encontrar si hay diferencias significativas entre grupos y testear la hipótesis sobre las diferencias en las medias de estos grupos. Para poder calcularla, se tendrán en cuenta dos tipos de varianza: 1) La varianza entre grupos (los valores promedio de cada grupo se comparan con la media total de los datos); y la varianza dentro del grupo (la varianza de cada dato en un grupo con el promedio dentro del mismo grupo).
+
+# Es importante que para poder realizar un Anova, se cumplan los siguientes requisitos o supuestos:
+# 1)Normalidad
+# 2)Independencia
+# 3)Homocedasticidad
+
+# Vamos a aprender primero como realizar la anova a mano paso a paso y luego utilizaremos la función de ANOVA en R.
+
+###################################################################
+##                  Anova a mano                                 ##
+###################################################################
+
+# Vamos a generar nuestros datos para hacer una Anova a mano. Supongamos que estamos experimentando con un tratamiento que permita a las plantulas de una planta crecer más rápido. Para esto tendremos dos grupos de plantas de una misma especie, unas a las que se les provea un compuesto "z" el cuál debería acelerar el crecimiento y otros a los que no. Llamaremos al grupo con el tratamiento "z" g1 y el otro grupo g2.
+
+#Generamos datos aleatorios del crecimiento de las plantas, la cual es nuestra variable dependiente
+
+g1 <- rnorm(30, 10, 2)
+
+g2 <- rnorm(30, 5, 2)
+
+y <- c(g1, g2) # Los concatenamos en un mismo vector
+
+x <- rep(c(1, 2), each = 30) # Creamos la variable de nuestros grupos o variable independiente, el 1 corresponde a las plantulas g1 o "z"
+
+plot(y)
+
+plot(x, y) # En este punto parecen haber diferencia entre el crecimiento del grupo 1 y 2
+
+# Ahora empezamos a realizar una ANOVA paso a paso. Para esto, hallaremos la suma de cuadrados y los grados de libertad, pues esto es igual a la varianza
+
+#Obtenemos los promedios de cad agrupo y total, los cuales necesitaremos para hallar la suma de los residuales al cuadrado (suma total de la distancia entre cada observación con la media elevada al cuadrado)
+
+tot.mean <- mean(y) # Obtenemos el promedio de todo el grupo
+abline(tot.mean, 0) 
+?abline
+# a = intercepto, b = pendiente, para una linea horizontal la pendiente (slope) = 0
+
+g1.mean <- mean(g1)
+g2.mean <- mean(g2) 
+
+abline(g1.mean, 0, col = "red") # media del grupo 1
+abline(g2.mean, 0, col = "green") # media del grupo 2
+
+# Calculamos la suma de cuadrados de los residuos, los cuales se dividen en suma de cuadrados totales (SST); el error de la suma de cuadrados (SSE), que se refiere a la varianza dentro de cada grupo que no se puede explicar; y la suma de cuadrados del tratamiento (SSA) o viarianza entre grupos
+
+# Suma de cuadrados total: SST = SSE + SSA
+
+SST <- sum((y - tot.mean) ^2) #Hallamos el SST sumando la resta entre la media de todos los datos con cada observación elevada al cuadrado
+
+# Suma de cuadrados residual
+
+SSE.g1 <- sum((g1 - g1.mean) ^2) # Realizamos el mismo paso para calcular el SST pero con el subconjunto de datos de cada grupo
+
+SSE.g2 <- sum((g2 - g2.mean) ^2)
+
+SSE <- SSE.g1 + SSE.g2 #sumamos los SSE de cada grupo
+
+# SSA
+
+SSA <- SST - SSE 
+
+# A partir de acá, vamos a calcular el estadístico F, el promedio de los cuadrados (MSA Y MSE), los grados de libertad y el valor de p (valor de significancia) para determinar si nuestros grupos son estadísticamente diferentes
+
+# MSA = SSA/k-1(grados de libertad)
+
+MSA <- SSA/1 #siendo k igual al número de grupos, que en nuestro caso es 2
+
+# MSE = SSE/k(n-1) (grados de libertad)
+
+MSE <- SSE/58 #en este caso, n es igual a 30 o número de observaciones por grupo
+
+# Estadístico F
+
+Fval <- MSA/MSE 
+Fval  
+
+# Valor de significancia p = qf(0.95, k-1, k(n-1))
+
+qf.sig <- qf(0.01, 1, 58)
+qf.sig
+
+# Usualmente, valores pequeños de p provienen de valores de F grandes, y estos a su vez sugieren una varianza mas grande entre grupos que dentro de cada grupo. En nuestro experimento, se puede concluir que la sustancia "z" está teniendo afectando el crecimiento de las plántulas de manera positiva.
+
+# Finalmente, podemos usar el R2 para determinar cuanta varianza en nuestros datos (y) está siendo explicada por la variable independiente (x)
+
+R2 = 1 - (SSE/SST)
+R2 #68% de la varianza en nuestros datos puede ser explicada por la variable x
+
+# Felicitaciones, hizo una ANOVA por usted mismo(a), paso a paso. De ahora en adelante, puede usar las funciones de R!
+
+##################################################################
+## Ejercicio
+
+# 1) Cree dos vectores de datos aleatorios de 40 observaciones que se diferencien por sus valores medias, las cuales vienen de una distribcuón normal. Finalmente concatene ambos vectores
+
+# 2) Cree un vector "y" en el cuál se repitan dos letras, cada una igual al número de observaciones de cada vector creado en el punto 1
+
+# 3) Realice una anova con los vectores creados anteriormente e identifique si la varianza de ambos grupos es significativamente diferente 
+
+## fin ejercicio
+############################################################################
+
+###################################################################
+##                  Anova con funciones de R                     ##
+###################################################################
+
+# En R, usamos la función aov() para hacer una anova rápidamente
+
+?aov()
+
+aov(y ~ x)
+
+# Si quieremos ver el valor de F y el valor de p, utilizamos la función summary
+
+summary(aov(y ~ x)) 
+
+# Vemos que el valor de F es igual al que calculamos anteriormente, y también el valor de p (Pr(>F)) es significativo y recahzamos la hipótesis nula H0. 
+
+# Hasta este punto, hemos realizado un anova de una vía, ya que solo determinamos el efecto de una sola variable independiente, lo que es igual a realizar una regresión lineal simple, ya que Anova es un método que ocurre dentro del modelo de la regresión lineal.
+
+summary(lm(y ~ x))
+
+###################################################################
+##                  Anova con datos reales                       ##
+###################################################################
+
+library(tidyverse)
+
+setwd("~/R/Nuevo curso")
+
+# En esta sección vamos a explorar el análisis de anova con un set de datos que contiene información taxonómica de especies vegetales, abundancia e información del diametro promedio de cada especie colectada en diferentes parcelas. Estos datos hacen parte de la fundación COLTREE, la cual se dedica al monitoreo de parcelas permanentes de vegetacion a nivel nacional, con el proposito de comprender el funcionamiento de los bosques en terminos de su productividad vegetal y su relacion con el ciclo del carbono. 
+
+dat <- read_csv("Species_Coltree.csv")
+
+# Utilizando la función de Anova en R, vamos a evaluar si existen diferencias en el diametro entre los géneros de plantas de una misma familia. En este ejemplo, vamos a trabajar con mas de dos grupos ya que es más frecuente utilizar otra prueba (T-Test) para comparar solo dos grupos.
+
+# Escogemos la familia Anacardiaceae
+
+familia <- dat %>% 
+  filter(N_Familia == "ANACARDIACEAE")
+
+# Visualizamos las medias de los géneros
+
+familia %>% 
+  ggplot(aes(x = Genero, y = DAP)) +
+  geom_boxplot()
+
+# Realizamos la Anova
+
+anova <- aov(familia$DAP ~ familia$Genero)
+
+summary(anova)
+
+# En este ejemplo, vemos que tenemos un valor de F bajo, y un valor de p>0.05, por lo que concluimos que el diametro entre los géneros de esta familia no difieren entré si significativamente. Sin embargo, con summary no obtenemos información sobre cuáles grupos se están diferenciando o no. Para esto, hacemos uso de una prueba post-hoc o "Tukey's Honest Significant Differences" (HSD) para ayudarnos a cuantificar cuales grupos difieren entre ellos.
+
+?TukeyHSD()
+
+TukeyHSD(anova)
+
+# Podemos ver un valor de p asociado a la interacción entre grupos. Vemos que los géneros que más difieren son Tapirira y Spondias, sin embargo no es significativo
+
+##################################################################
+## Ejercicio
+
+# Realice una Anova para evaluar si existen diferencias en el diametro entre las familias de dos parcelas diferentes y realice una prueba post hoc
+
+## fin ejercicio
+############################################################################
