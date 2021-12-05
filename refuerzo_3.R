71abd0df5d06b5785803e2f73fb179928d64eb3a
Gio-Von-derFluss <yovanny_54@hotmail.com>
2021-12-05 12:19:01 -0500

    updated

---
+++ refuerzo_3.R
@@ -0,0 +1,29 @@
+####################################################################
+##                                                                ##
+##  Análisis de datos utilizando el lenguaje de programación R:   ##
+##  una experiencia de aprendizaje virtual en el aula invertida   ##
+##                                                                ##
+##                 Proyecto INNOVA-TIC 2021                       ##
+##                                                                ##
+## Creado por:     Björn Reu (breu@uis.edu.co )                   ##
+##                 Yovanny Duran Barajas                          ##
+##                 Zarith Tatiana Villamizar                      ##
+##                 Silvia Fernanda Ardila Hurtado                 ## 
+##                 Sergio Andres Bolivar-Santamaria               ##
+##                                                                ##
+##  con el apoyo de la Vicerrectoría Académica,                   ##
+##  Universidad Industrial de Santander                           ##
+##                                                                ##
+####################################################################
+##                                                                ##
+##                     ***   Unidad 3  ***                        ##
+##                     Ejercicios y refuerzo                      ##
+##                                                                ##
+####################################################################
+
+dat_env <- read.csv("env_Paramos_Colombia.csv")
+dat_pa <- read.csv("pa_Paramos_Colombia.csv")
+
+####################################################################
+##                           Sesión 1                             ##
+####################################################################
