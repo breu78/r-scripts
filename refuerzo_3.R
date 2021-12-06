1ab00d7773e5a3889173903c9ae4fb5585e0f605
Gio-Von-derFluss <yovanny_54@hotmail.com>
2021-12-05 23:02:38 -0500

    upload

--- refuerzo_3.R
+++ refuerzo_3.R
@@ -21,7 +21,6 @@
 ##                                                                ##
 ####################################################################
 
-reg <- read.csv("regression.csv")
 anov <- read.csv("anova.csv")
 
 # Lea el archivo anova.csv.  Estos son los resultados de un experimento que compara la producción (en peso seco de la planta) obtenida en un control y dos tratamientos diferentes. 
@@ -30,7 +29,9 @@ anov <- read.csv("anova.csv")
 
 # 2. Realice una ANOVA y reporte la suma de los cuadrados explicando para los grupos en relación con la suma de cuadrados de los residuos. 
 
-# 3. Calcule el valor F y utilice la prueba de Tukey post-hoc utilizando la función TukeyHSD() para evaluar si hay diferencias significativas entre las medias   
+# 3. Calcule el valor F y utilice la prueba de Tukey post-hoc utilizando la función TukeyHSD() para evaluar si hay diferencias significativas entre las medias  
+reg <- read.csv("regression.csv")
+
 # Lea el conjunto de datos regression.csv. Estos datos son del géiser Old Faithful en el Parque Nacional de Yellowstone. La columna 1 muestra la duración de la erupción y la columna 2 el tiempo de espera entre las erupciones. 
 
 # 1. Muestre la relación entre las dos variables en un diagrama de dispersión, y preséntelo bien ( título, etiquetas de los ejes , etc. ). 
